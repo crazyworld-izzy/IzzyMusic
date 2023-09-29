fad22440fff17a608c7eb38bd02dfd667165e0aa
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2023-09-29 16:05:14 +0530

    Update thumbnails.py

--- IzzyMusic/utils/thumbnails.py
+++ IzzyMusic/utils/thumbnails.py
@@ -81,11 +81,11 @@ async def gen_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (6, 6), f"Powered By:- © Insane & ® ROCKS", fill="Yellow", font=name_font
+            (6, 6), f"Powered By:- © moni", fill="Yellow", font=name_font
         )
         draw.text(
             (600, 150),
-            f"By Insane",
+            f"By moni",
             fill="white",
             stroke_width=2,
             stroke_fill="yellow",
@@ -131,7 +131,7 @@ async def gen_thumb(videoid):
         )
         draw.text(
             (600, 550),
-            f"Owner : Dr Asad Ali",
+            f"Owner : moni",
             fill="white",
             stroke_width=1,
             stroke_fill="white",
